6b9c9369ce67d1913ec9f4a2ca9d61755166c796
tyulyukov <m.tyulyukov@tech.lab325.com>
2023-07-02 01:58:46 +0300

    lc 27, 88 + proj

---
+++ lc-27-remove-element/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        var num = 0;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (nums[i] == val)
+            {
+                for (int j = i; j < nums.Length - 1; j++)
+                {
+                    nums[j] = nums[j + 1];
+                }
+            }
+            else num++;
+        }
+
+        return num;
+    }
+    
+    public int RemoveElementBest(int[] nums, int val)
+    {
+        var num = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val) continue;
+            
+            nums[num] = nums[i];
+            num++;
+        }
+        return num;
+    }
+}
---
+++ lc-88-merge-sorted-array/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        n--;
+        for (; n >= 0; n--)
+        {
+            nums1[m + n] = nums2[n];
+        }
+        
+        Array.Sort(nums1);
+    }
+}
