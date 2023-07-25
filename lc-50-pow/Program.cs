1a08f9404c418c335bb5eb00974e0f56dd575628
tyulyukov <makstyulyukov@gmail.com>
2023-07-26 01:11:14 +0300

    lc 50

---
+++ lc-50-pow/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var s = new Solution();
+Console.WriteLine(s.MyPow(2, -2147483648));
+
+public class Solution {
+    public double MyPow(double x, int n)
+    {
+        return n switch
+        {
+            0 => 1,
+            < 0 => InternalPositivePow(1 / x, -(long)n),
+            _ => InternalPositivePow(x, n)
+        };
+    }
+
+    private static double InternalPositivePow(double x, long n)
+    {
+        if (n == 1) return x;
+        var halfPow = InternalPositivePow(x, n / 2);
+        return n % 2 == 0 ? halfPow * halfPow : halfPow * halfPow * x;
+    }
+}
